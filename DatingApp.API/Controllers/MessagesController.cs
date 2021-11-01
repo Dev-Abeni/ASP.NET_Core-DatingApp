bab44027211c9f880b4f24f696f532afe06fe12b
Abenezer Mulugeta <abenezermulugeta31@gmail.com>
2021-11-01 14:20:22 +0300

    Implement the GetMessage method.

---
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using DatingApp.API.Data;
+using DatingApp.API.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    [ServiceFilter(typeof(LogUserActivity))]
+    [Authorize]
+    [Route("api/users/{userId}/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IDatingRepository _repository;
+        private readonly IMapper _mapper;
+        public MessagesController(IDatingRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}", Name = "GetMessage")]
+        public async Task<IActionResult> GetMessage(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var messageFromRepo = await _repository.GetMessage(id);
+
+            if (messageFromRepo == null)
+                return NotFound();
+
+            return Ok(messageFromRepo);
+        }
+    }
+}
